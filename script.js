718f874e5f5e90eb8fa35a7032c5d9d9fe7f24e7
itsyuktha <princyyuktha583@gmail.com>
2026-08-11 18:49:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const taskInput = document.getElementById("taskInput");
+const addButton = document.getElementById("addButton");
+const taskList = document.getElementById("taskList");
 
+addButton.addEventListener("click", function () {
+    const taskText = taskInput.value.trim();
+
+    if (taskText === "") {
+        return;
+    }
+
+    const listItem = document.createElement("li");
+
+    listItem.textContent = taskText;
+
+    taskList.appendChild(listItem);
+
+    taskInput.value = "";
+});
